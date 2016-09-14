180c44e439046ebd5c5e25a318464334ef24d0e2
leniwwwiy <leniwwwiy@gmail.com>
2016-09-14 11:58:56 +0700

    Update MyBd.h

--- ConsoleApplication4/ConsoleApplication4/MyBd.h
+++ ConsoleApplication4/ConsoleApplication4/MyBd.h
@@ -10,55 +10,55 @@
 using namespace std;
 
 struct bd{
-	int in, cost; // доход и остаток соотв.
-	vector<int> out;// список расходов
-	string name;// имя
+	int in, cost;
+	vector<int> out;
+	string name;
 	bd(string &s, int i, int c) :name(s), in(i), cost(c){}
-	bd() :name("unknown"), in(0), cost(0){}// по умолчанию конструктор
-	void clr(){ in = 0; cost = 0; out.clear(); name = "unknown"; }// функция-метод "почистить" обнуляет всё
-	void pr(){// функция-метод вывода на экран
-		cout << name << "\t Доход: " << in << "\t Остаток:" << cost << "\t Расходы:";
+	bd() :name("unknown"), in(0), cost(0){}
+	void clr(){ in = 0; cost = 0; out.clear(); name = "unknown"; }
+	void pr(){
+		cout << name << "\t Г„Г®ГµГ®Г¤: " << in << "\t ГЋГ±ГІГ ГІГ®ГЄ:" << cost << "\t ГђГ Г±ГµГ®Г¤Г»:";
 		if (out.size() == 0)
-			cout << "расходов пока нет.";
+			cout << "Г°Г Г±ГµГ®Г¤Г®Гў ГЇГ®ГЄГ  Г­ГҐГІ.";
 		for (int j = 0; j<out.size(); ++j)
 			cout << out[j] << ' ';
 		cout << endl;
 	}
-	void sca(ifstream &i){// функция-метод считывания элемента из указанного входного потока
+	void sca(ifstream &i){
 		clr();
 		string s;
 		int t;
 		i >> name >> in >> cost;
-		getline(i, s);// читаем всю оставшуюся строку
-		stringstream ss(s);// запихиваем в строковый потоковый буфер 
+		getline(i, s);
+		stringstream ss(s);
 		cost = in;
-		ss >> t; // и из него по одному инты вытягиваем как обычно
+		ss >> t;
 		while (ss){
 			cost -= t;
 			out.push_back(t);
 			ss >> t;
 		}
 	}
-	void sc(){// просто считать имя и доход для создания новой учетки с расчётом остатка
+	void sc(){
 		cin >> name >> in;
 		cost = in;
 	}
-	void add(int o){// добавить расход с расчётом остатка
+	void add(int o){
 		out.push_back(o);
 		cost -= o;
 	}
-	void editin(int newin){// изменить доход и рассчитать остаток
+	void editin(int newin){
 		int d = newin - in;
 		cost += d;
 		in = newin;
 	}
 };
 
-bool operator < (const bd &l, const bd &r) {// оператор сравнения для элементов БД
+bool operator < (const bd &l, const bd &r) {
 	return l.name<r.name;
 }
 
-set<bd> load(const string &nof){// загрузить все учётки которые в файле
+set<bd> load(const string &nof){
 	ifstream in(nof);
 	set<bd> data;
 	if (!in)
